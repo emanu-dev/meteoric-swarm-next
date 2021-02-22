597ef718dffc0b10ab25bbe5084f4bd234cf058c
Emanuel Prado <emanu-dev@DESKTOP-JJOEB1K.localdomain>
2021-02-22 20:32:02 -0300

    Update cursor style

--- src/components/cursor/index.js
+++ src/components/cursor/index.js
@@ -17,17 +17,43 @@ const Wrapper = styled.div`
 const CursorPointer = styled.div`
   border: 2px solid ${props => props.theme.colors.textContrast};
   border-radius: 50%;
+  border-left: 1px solid ${props => props.theme.colors.textContrast};
+  border-right: 1px solid ${props => props.theme.colors.textContrast};
   background-color: rgba(0,0,0,.5);
   height: 40px;
   width: 40px;
   transform: rotateZ(45deg);
-  transition: border-radius 150ms ease-in;
+  transition: all 150ms ease-out;
   position: relative;
   top: 10px;
   left: 10px;
 
+  &:after {
+    background-color: rgba(255,255,255,.25);
+    content: ' ';
+    border-radius: 50%;
+    height: 5px;
+    left: calc(50% - 3px);
+    position: absolute;
+    top: calc(50% - 3px);
+    transition: all 150ms ease-out;
+    width: 5px; 
+  }
+
   &.--active {
     border-radius: 0;
+    height: 30px;
+    width: 30px;    
+    top: 8px;
+    left: 8px;  
+
+    &:after {
+      border-radius: 0%;
+      height: 15px;
+      left: calc(50% - 8px);
+      top: calc(50% - 8px);
+      width: 15px; 
+    }      
   }
 `
 
